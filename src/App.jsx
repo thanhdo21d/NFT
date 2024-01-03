91de177bc224a9b6302bf2d04cdb10dc6a8cddc8
Vu Thanh Do (DMVN) <vu.thanh.do.a7y@ap.denso.com>
2024-01-03 11:00:51 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
       <Header />
+      <ToastContainer />
     </>
   );
 }
