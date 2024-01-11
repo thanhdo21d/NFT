c7abbdbe82a5a935bfd5a81eae7f5a83e5ed553e
Vu Thanh Do (DMVN) <vu.thanh.do.a7y@ap.denso.com>
2024-01-11 13:21:12 +0700

    updTE

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 import Header from "./components/Header";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
+  const analytics = Analytics({
+  })
   return (
     <>
       <Header />
       <ToastContainer />
+      <Analytics />
     </>
   );
 }
